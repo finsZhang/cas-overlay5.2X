49f737eb6bb1e8cf579406856af168868d646b08
finsZhang <zhangfengzhou110@163.com>
2018-10-11 18:33:17 +0800

    Druid性能监控

--- src/main/java/com/ai/cas/stat/DruidMonitorConfig.java
+++ src/main/java/com/ai/cas/stat/DruidMonitorConfig.java
@@ -1,11 +1,20 @@
 package com.ai.cas.stat;
 
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +25,33 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DruidMonitorConfig {
+
+
+    /**
+     * 使用连接池dataSource
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        List<Filter> filterList=new ArrayList<>();
+        filterList.add(wallFilter());
+        druidDataSource.setProxyFilters(filterList);
+        return druidDataSource;
+    }
+    @Bean
+    public WallFilter wallFilter(){
+        WallFilter wallFilter=new WallFilter();
+        wallFilter.setConfig(wallConfig());
+        return  wallFilter;
+    }
+    @Bean
+    public WallConfig wallConfig(){
+        WallConfig config =new WallConfig();
+        config.setMultiStatementAllow(true);//允许一次执行多条语句
+        config.setNoneBaseStatementAllow(true);//允许非基本语句的其他语句
+        return config;
+    }
     /**
      * 注册ServletRegistrationBean
      * @return
